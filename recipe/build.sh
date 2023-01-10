ebd0cc75821fb3e96d8ce2375a34b75b2f9beaef
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2023-01-10 10:59:50 +0100

    Add tests (#4)
    * use ../src
    
    * update versions on recipe
    
    * update patch for conda 22.11.1
    
    * quote some strings
    
    * copy whole repo
    
    * rename before overwriting files from cache
    
    * set -eux
    
    * symlink Apple's codesign into BUILD_PREFIX
    
    * make sure we use the classic solver in case condarc configs get in the way
    
    * Add workflow
    
    * fix runner os
    
    * rethink paths
    
    * revert
    
    * try other provisioning
    
    * no env file
    
    * give env name
    
    * change shell
    
    * add .gitattributes
    
    * fix rename command on windows
    
    * verbosity up
    
    * rename some dirs for consistency
    
    * Create CODEOWNERS
    
    * Use setup-miniconda
    
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,23 @@
+set -euxo pipefail
+
 # patched conda files
 # new files in patches need to be added here
 for fname in "core/path_actions.py" "utils.py"; do
-  cp conda_src/conda/${fname} $SP_DIR/conda/${fname}
+  mv "$SP_DIR/conda/${fname}" "$SP_DIR/conda/${fname}.bak"
+  cp "conda_src/conda/${fname}" "$SP_DIR/conda/${fname}"
 done
+
+# make sure pyinstaller finds Apple's codesign first in PATH
+# some base installations have 'sigtool', which ships a
+# 'codesign' binary that might shadow Apple's codesign
+if [[ $target_platform == osx-* ]]; then
+  ln -s /usr/bin/codesign "$BUILD_PREFIX/bin/codesign"
+fi
 
 # -F is to create a single file
 # -s strips executables and libraries
-pyinstaller conda.exe.spec
-mkdir -p $PREFIX/standalone_conda
-mv dist/conda.exe $PREFIX/standalone_conda
+pyinstaller --clean --log-level=DEBUG src/conda.exe.spec
+mkdir -p "$PREFIX/standalone_conda"
+mv dist/conda.exe "$PREFIX/standalone_conda"
 # clean up .pyc files that pyinstaller creates
-rm -rf $PREFIX/lib
+rm -rf "$PREFIX/lib"
