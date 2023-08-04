5d3e9c29e3e7955c708acfac32433fd7ba65b8b5
jaimergp <jaimergp@users.noreply.github.com>
2023-08-04 13:27:54 +0200

    use pytest

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import sys
+
+
+CONDA_EXE = os.environ.get(
+    "CONDA_STANDALONE",
+    os.path.join(sys.prefix, "standalone_conda", "conda.exe")
+)
+
+
+def run_conda(*args, **kwargs):
+    args = (CONDA_EXE, *args)
+    return subprocess.run([CONDA_EXE, *args], **kwargs)
+
+
+def test_new_environment(tmp_path):
+    p = run_conda("create", "-p", tmp_path / "env", "-y", "-c", "conda-forge", "libzlib")
+    p.check_returncode()
+    assert list((tmp_path / "env" / "conda-meta").glob("libzlib-*.json"))
