94c193f88fe11fe6ca5df09c3ab341226da8fde0
jaimergp <jaimergp@users.noreply.github.com>
2024-06-24 12:10:04 +0200

    try mingw

--- src-nuitka/conda.nuitka.py
+++ src-nuitka/conda.nuitka.py
@@ -16,6 +16,8 @@
 # nuitka-project: --include-package-data=menuinst:*.json
 # nuitka-project-if: {OS} == "Darwin":
 #    nuitka-project: --include-package-data=menuinst:*launcher*
+# nuitka-project-if: {OS} == "Windows":
+#    nuitka-project: --mingw64
 # nuitka-project: --no-deployment-flag=self-execution
 
 """
