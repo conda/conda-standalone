e24af06e535fbd57289a593752cce130c2b21e50
Marco Esters <mesters@anaconda.com>
2025-10-28 09:00:15 -0700

    Add flags to add condabin and conda libraries to PATH

--- src/conda_constructor/cli.py
+++ src/conda_constructor/cli.py
@@ -118,6 +118,16 @@ def _add_windows_path(parser: ArgumentParser) -> None:
         default=None,
         help="Removes a prefix from the Windows PATH.",
     )
+    parser.add_argument(
+        "--condabin",
+        action="store_true",
+        help="Adds the condabin directory under PREFIX to PATH.",
+    )
+    parser.add_argument(
+        "--condalibs",
+        action="store_true",
+        help="Adds PREFIX and additional libraries under PREFIX to PATH.",
+    )
 
 
 def configure_parser(parser: ArgumentParser) -> None:
@@ -185,6 +195,8 @@ def execute(args: Namespace) -> None | int:
             kwargs.update(
                 {
                     "add": args.append or args.prepend,
+                    "condabin": args.condabin,
+                    "condalibs": args.condalibs,
                     "append": args.append is not None,
                     "remove": args.remove,
                 }
