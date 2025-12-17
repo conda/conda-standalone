8abe00caf3d0892705471164faa94549bc24ba38
Robin <randersson@anaconda.com>
2025-12-17 08:23:04 -0500

    Added missing check for prepend

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -41,7 +41,8 @@ def test_windows_add_remove_path(
             tmp_path / "Library" / "bin",
             tmp_path / "Scripts",
         ]
-        expected_paths.reverse()
+        if append_or_prepend == 'prepend':
+            expected_paths.reverse()
     else:
         expected_paths = [tmp_path]
     create_command = [
